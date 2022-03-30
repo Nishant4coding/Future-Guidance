a57a8c021d535e7c589292d8458c8b5689890c05
Nishant4coding <92370893+Nishant4coding@users.noreply.github.com>
2022-03-30 11:53:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let namasteBtn = document.querySelector('button')
-namasteBtn.addEventListener('click', inputMsg);
-
-function inputMsg(){
-    let name = prompt('ENTER THE NAME OF STUDENT');
-    namasteBtn.textContent = 'roll no.1:' + name;
-}
